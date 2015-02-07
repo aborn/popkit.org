004899887a45c5728927e28950289c31510b4639
蒋国宝 <loveaborn@foxmail.com>
2015-02-08 00:12:18 +0800

    modify start and stop script

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-nohup ./bin/www > ./logs/run.log &
+nohup ./bin/www > ./logs/run.log 2>&1 &
+echo $! > ./proc/run.pid
--- stop.sh
+++ stop.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo kill `ps -ef |grep node |grep www |awk '{print $2}'`
+# sudo kill `ps -ef |grep node |grep www |awk '{print $2}'`
+sudo kill -9 `cat ./proc/run.pid`
